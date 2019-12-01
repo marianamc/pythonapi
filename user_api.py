82f9f454911459dc1356a0efe3bdbf00ddcfeca8
Mariana Mendes <maritomaverick@gmail.com>
2019-12-01 11:16:07 -0300

    Create methods to validate the info
    Create separated methods to validate user and
    password and to add or disable users on data.txt

--- user_api.py
+++ user_api.py
@@ -3,18 +3,38 @@
 import sys
 import base64
 
+def validate_password(password):
+    return True
+	
+def validate_login(login):
+    return True
+
+def add_user (args):
+    decoded_password = str(base64.b64decode(args [3]), 'utf-8')
+    if validate_password(decoded_password) and validate_login(args[2]):
+        info = 'ADD "' + args[1] + '", "' + args[2] + '", "' + decoded_password + '"'
+        return info
+    else:
+        sys.exit(1)
+
+
+def disable_user (args):
+    info = 'DISABLE "' + args[1] +'"'
+    return info;
+	
+
 def main():
     args = sys.argv[1:]
 
     if not args:
-        print('usage: --add name login password or --del login')
+        print('usage: --add <name> <login> <password> or --del <login>')
         sys.exit(1)
 
     if args[0] == '--add':
-        info = 'ADD "' + args[1] + '", "' + args[2] + '", "' + str(base64.b64decode(args [3]), 'utf-8') + '"'
+        info = add_user(args)
 
     if args[0] == '--del':
-        info = 'DISABLE "' + args[1] +'"'
+        info = disable_user(args)
 
     with open('data.txt', 'a') as file:
         file.write(info + '\n')
